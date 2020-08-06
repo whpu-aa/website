134e4fb726097fb6bc335f6cbfd5a5ff3c908d61
liu839 <710374839@qq.com>
2020-08-06 09:21:02 +0800

    尝试稍作修改文字内容

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="欢迎来到武汉轻工大学算法协会的网站" />
   </div>
 </template>
 
