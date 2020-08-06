91d02f92fe71091b93f4fd28e8ee69e80dd3e9fa
crupest <crupest@outlook.com>
2020-08-06 15:09:35 +0800

    再次修改主页的消息。

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to our website!" />
+    <HelloWorld msg="Welcome to our website, hahaha!" />
   </div>
 </template>
 
