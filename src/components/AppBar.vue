ddd0fb51b0304f0f51c243b586d9f46fea324902
crupest <crupest@outlook.com>
2020-08-17 23:50:30 +0800

    将AppBar提取为一个单独的组件。

---
+++ src/components/AppBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="nav">
+    <router-link to="/">主页</router-link>|
+    <router-link to="/about">关于我们</router-link>
+  </nav>
+</template>
+
+<style scoped>
+.nav {
+  padding: 30px;
+}
+
+.nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  text-decoration: none;
+}
+
+.nav a.router-link-exact-active {
+  color: #42b983;
+  text-decoration: none;
+}
+</style>
