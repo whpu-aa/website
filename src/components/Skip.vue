7b7e908965ab19b88d8fdc7b9b224ad8c3da8838
WithBQ <1406840143@qq.com>
2020-08-23 22:17:03 +0800

    增加可用来跳转的组件，组件内容为图片加文本。修改新闻文章组件中部分样式并添加其他新闻部分

---
+++ src/components/Skip.vue
@@ -0,0 +1,61 @@
+<template>
+  <a
+    class="toMoreArticles"
+    :class="{ over: number == 0 }"
+    :href="href"
+    target="_block"
+    @mouseover="mouseOver"
+    @mouseleave="mouseLeave"
+  >
+    <img class="moreArticlesIcon" :src="require('@/assets/' + src)" />
+    <div class="articleContents">{{ articleContents }}</div>
+  </a>
+</template>
+
+<script>
+export default {
+  name: "Skip",
+  data() {
+    return {
+      number: null
+    };
+  },
+  props: {
+    href: String,
+    src: String,
+    articleContents: String
+  },
+  methods: {
+    mouseOver() {
+      this.number = 0;
+    },
+    mouseLeave() {
+      this.number = null;
+    }
+  }
+};
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+  color: black;
+  margin: 10px;
+}
+.over {
+  color: aqua;
+}
+.toMoreArticles {
+  display: flex;
+  justify-content: flex-start;
+}
+.moreArticlesIcon {
+  width: 150px;
+  height: 75px; /*图片大小根据具体情况调整 */
+  object-fit: cover;
+  margin: 10px;
+}
+.articleContents {
+  margin: 10px;
+}
+</style>
--- src/views/NewsArticles.vue
+++ src/views/NewsArticles.vue
@@ -5,14 +5,32 @@
       <pre><p :key="index" v-for="(item, index) in msg_center" class="msgcenter">{{ item }}</p></pre>
       <pre><p :key="index" v-for="(item, index) in msg_left" class="msgleft">{{ item }}</p></pre>
     </div>
-    <div class="morearticle">
-      更多新闻
+    <div class="rightPart">
+      <div class="morearticle">
+        <p>其他新闻</p>
+        <!--跳转至其他文章-->
+        <div class="articleList">
+          <skip :src='skip_src' :href='skip_href' :articleContents='articles_contents'></skip>
+          <skip :src='skip_src' :href='skip_href' :articleContents='articles_contents'></skip>
+          <skip :src='skip_src' :href='skip_href' :articleContents='articles_contents'></skip>
+        </div>
+        <!--跳转到新闻目录-->
+        <a class="readMore" href="http://www.baidu.com" target="_block">阅读更多新闻</a>
+      </div>
+      <!--跳转至其他页面，例如日常题目，协会介绍等等-->
+      <div class="otherRecommended">
+        <p>相关推荐</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Skip from "../components/Skip";
 export default {
+  components: {
+    "skip": Skip
+  },
   data() {
     return {
       title: "文章标题",
@@ -25,11 +43,15 @@ export default {
         "  据CNN报道，拜登在演讲中表示：“我们团结在一起，将能够越过美国当前的这一充满黑暗的时节。如果你们委任我成为美国总统，我将会发挥出我们最好的一面，而不是最坏的一面。现在是时候让我们团结起来了。",
         "  据美联社报道，为其四天的美国民主党全国代表大会于17日正式开始。19日，来自美国50个州、哥伦比亚特区和海外领地的57个代表团逐一远程唱票。在北卡罗来纳州宣布计票结果后，拜登获得2448张党代表票，超过提名所需2374票，正式成为2020年美国总统选举民主党候选人，参加11月3日的总统大选，与共和党籍、74岁的现任总统特朗普角逐下一任美国总统",
         "  拜登1942年11月20日出生于美国宾夕法尼亚州，律师出身，1970年踏入政界，1972年首次当选联邦参议员，曾任参议院司法委员会和对外关系委员会主席，在奥巴马执政时期任美国副总统。当前是他第三次竞选总统。（海外网 李芳 张霓）"
-      ]
+      ],
+      skip_src: "logo.png",
+      skip_href: "http://www.baidu.com",
+      articles_contents: "文章内容"
     };
   }
 };
 </script>
+
 
 <style scoped>
 .NewsArticles {
@@ -44,21 +66,52 @@ export default {
 .article {
   border-radius: 3%;
   height: auto;
+  min-height: 700px;
   flex-basis: 50%;
   background-color: rgb(211, 211, 211); /* 颜色未定 ,后期统一背板颜色 */
 }
 .morearticle {
   border-radius: 3%;
   flex-basis: 30%;
+  height: auto;
   background-color: rgb(211, 211, 211);
 }
 pre {
   white-space: pre-wrap; /*css-3*/
+}
+a {
+  text-decoration: none;
+  color: black;
+  margin: 10px;
 }
 .msgcenter {
   text-align: center;
 }
 .msgleft {
   text-align: left;
+}
+.rightPart {
+  display: flex;
+  height: auto;
+  flex-basis: 30%;
+  flex-direction: column;
+}
+.articleList {
+  display: flex;
+  flex-direction: column;
+  height: auto;
+  margin-bottom: 10px;
+}
+.readMore {
+  border-radius: 3%;
+  display: block;
+  min-height: 30px;
+  height: auto;
+  background-color: rgb(180, 180, 180);
+}
+.otherRecommended {
+  border-radius: 3%;
+  margin-top: 50px;
+  background-color: rgb(211, 211, 211);
 }
 </style>
