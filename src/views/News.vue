398a8238dd9b5a5d3ecf8ae2f551de2afe302f44
liu839 <710374839@qq.com>
2020-08-29 20:24:38 +0800

    主页结构性完善(未全部完成)

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,23 +1,40 @@
 <template>
   <div :class="main">
     <el-header>
-      <el-row>
-        <el-input
-          v-model="input"
-          placeholder="请输入内容"
-          class="search"
-        ></el-input>
-        <el-button icon="el-icon-search" circle></el-button>
-      </el-row>
+      <el-input
+        v-model="input"
+        placeholder="请输入内容"
+        class="search"
+      ></el-input>
+      <el-button icon="el-icon-search" circle></el-button>
     </el-header>
+    <el-main>
+      <more-article-item
+        :iconSrc="skip_icon_src"
+        :targetHref="skip_href"
+        :articleContents="articles_contents"
+      ></more-article-item>
+      <more-article-item
+        :iconSrc="skip_icon_src"
+        :targetHref="skip_href"
+        :articleContents="articles_contents"
+      ></more-article-item>
+    </el-main>
   </div>
 </template>
 
 <script>
+import MoreArticleItem from "../components/MoreArticleItem";
 export default {
+  components: {
+    MoreArticleItem
+  },
   data() {
     return {
-      searchtext: ""
+      searchtext: "",
+      skip_icon_src: "logo.png",
+      skip_href: "http://www.baidu.com",
+      articles_contents: "文章内容"
     };
   }
 };
@@ -26,9 +43,17 @@ export default {
 .main {
   display: flex;
 }
+.el-input {
+  width: 300px;
+}
+.el-button {
+  margin-top: 10px;
+  margin-left: 10px;
+  width: 40px;
+  height: 40px;
+}
 .el-header {
-  display: flex;
-  justify-content: space-between;
+  justify-content: center;
   background-color: #b3c0d1;
   color: #333;
   text-align: center;
