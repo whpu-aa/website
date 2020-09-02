1d279a04800156173ccd184b786c4588458d7b9a
Mamba-ZJP <1330472951@qq.com>
2020-09-02 15:50:33 +0800

    添加部门介绍的逻辑部分和数据

--- src/views/GroupIntroduce.vue
+++ src/views/GroupIntroduce.vue
@@ -3,11 +3,28 @@
     <!--左边点击的区域-->
     <div class="left">
       <ul class="rocker">
-        <li :key="index" v-for="(item, index) in block">
+        <li :key="index" v-on:click="change(index)" v-for="(item, index) in block">
           <a v-bind:href="item.url" target="myframe">{{ item.ename }}</a>
         </li>
       </ul>
     </div>
+
+    <div class="right">
+      <table > 
+        <!-- v-bind:key="index" v-for="(item, index) in part" -->
+        <tr>{{ part[0] + ":" }}</tr>
+        <tr>{{ department[current].introduction }}</tr>
+        <div>------------------------------------------------------------------------------</div>
+        <tr>{{ part[1] + ":" }}</tr>
+        <tr>{{ department[current].manager }}</tr>
+        <div>------------------------------------------------------------------------------</div>
+        <tr>{{ part[2] + ":" }}</tr>
+        <tr :key="index" v-for="(name, index) in department[current].member">
+          <td>{{ name }}</td>
+        </tr>
+      </table>
+    </div>
+
     <!--中间显示窗口-->
     <div class="cen">
       <iframe
@@ -19,25 +36,58 @@
       ></iframe>
     </div>
     <div class="clear"></div>
+
+    
   </div>
 </template>
+
 <script>
 export default {
   name: "GroupPage",
+  methods: {
+    change: function(index){
+      this.current = index;
+      console.log(this.current);
+    }
+  },
+  
   data() {
     return {
+      current: 0,
       block: [
-        { ename: "管理组", url: "http://www.baidu.com" },
-        { ename: "运行组", url: "http://www.taobao.com" },
-        { ename: "宣传组", url: "http://www.taobao.com" },
-        { ename: "人员介绍", url: "http://www.taobao.com" },
-        { ename: "five", url: "http://www.taobao.com" }
-      ]
+        { ename: "管理组"},
+        { ename: "运行组"},
+        { ename: "宣传组" },
+        { ename: "人员介绍"},
+        { ename: "five"}
+      ],
+      part: ["部门介绍", "负责人介绍", "成员介绍"],
+
+      department: [{
+        introduction: "管理组的日常主要任务是......",
+        manager: "周云",
+        member: ["张翔轩", "袁菲", "邵嘉林", "黄鑫鑫"],
+      },{
+        introduction: "运行组的日常主要任务是......",
+        manager: "魏锦明",
+        member: ["张翔轩", "袁菲", "邵嘉林", "黄鑫鑫"],
+      },{
+        introduction: "宣传组的日常主要任务是......",
+        manager: "刘德",
+        member: ["张翔轩", "袁菲", "邵嘉林", "黄鑫鑫"],
+      }]
+
     };
   }
 };
 </script>
+
 <style scoped>
+
+.right {
+  background-color: white;
+  height: 500px;
+}
 .cen {
   /*主页中间最大的div*/
   width: 900px;
@@ -46,7 +96,7 @@ export default {
   float: left;
   position: relative;
   opacity: 0.8;
-  background: url("../assets/group_pic/snow.png");
+  /* background: url("../assets/group_pic/snow.png"); */
   border: solid 10px white;
   border-radius: 10px;
 }
