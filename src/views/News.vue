b44bfc6dd98d605baa25c5c60e382549fe67ee59
unknown <710374839@qq.com>
2020-09-06 20:29:57 +0800

    删除字体设置

--- src/views/News.vue
+++ src/views/News.vue
@@ -88,10 +88,6 @@ export default {
 };
 </script>
 <style scoped>
-* {
-  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
-    "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
-}
 .el-input {
   width: 300px;
 }
