ecef4be9d74e91ba91a1a41fcdf4e1ba4e36ca4d
jeffyangcoder <872802292@qq.com>
2021-05-25 20:30:09 +0800

    完成了步骤一和步骤二

--- FrontEnd/src/components/AppBar.vue
+++ FrontEnd/src/components/AppBar.vue
@@ -1,40 +1,14 @@
 <template>
-  <!--  <nav class="nav">-->
-  <!--    <img class="logo" src="@/assets/logo.png" />-->
-  <!--    <router-link to="/">主页</router-link>-->
-  <!--    <router-link to="/Login">登录</router-link>-->
-  <!--    <router-link to="/SignUp">注册</router-link>-->
-  <!--    <router-link to="/about">关于我们</router-link>-->
-  <!--    <router-link to="/NewsArticles">新闻文章</router-link>-->
-  <!--    <router-link to="/GroupIntroduce">部门介绍</router-link>-->
-  <!--    <router-link to="/News">新闻</router-link>-->
-  <!--    <router-link to="/updateCode">上传代码</router-link>-->
-  <!--    <router-link to="/showcode">展示代码</router-link>-->
-  <!--  </nav>-->
-  <div>
-    <div class="nav">
-      <img class="logo" src="@/assets/logo.png" />
-      <el-tabs v-model="activeName" @tab-click="handleClick">
-        <el-tab-pane
-            v-for="(item ,index) in site"
-            :name="item"
-            :label="item"
-            :key="index"
-        ></el-tab-pane>
-      </el-tabs>
-    </div>
-      <div v-if="activeName ==='主页'">切换到Home</div>
-      <div v-else-if="activeName ==='登录'">切换到Login</div>
-      <div v-else-if="activeName === '注册'">切换到SingUp</div>
-      <div v-else-if="activeName === '关于我们'">切换到About</div>
-      <div v-else-if="activeName === '新闻介绍'">切换到News</div>
-      <div v-else-if="activeName === '部门介绍'">切换到Group</div>
-      <div v-else-if="activeName === '新闻'">切换到News</div>
-      <div v-else-if="activeName === '上传代码'">切换到updateCode</div>
-      <div v-else-if="activeName === '展示代码'">切换到showCode</div>
-    <div>
-
-    </div>
+  <div class="nav">
+    <img class="logo" src="@/assets/logo.png"/>
+    <el-tabs v-model="activeName" @tab-click="tabClick">
+      <el-tab-pane
+          v-for="(item ,index) in site"
+          :name="item"
+          :label="item"
+          :key="index"
+      ></el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
@@ -42,14 +16,17 @@
 export default {
   data() {
     return {
-      activeName: "Home",
-      site:['主页','登录','注册','关于我们','新闻介绍','部门介绍','新闻','上传代码','展示代码'],
+      activeName: "主页",
+      site: ['主页', '登录', '注册', '关于我们', '新闻介绍', '部门介绍', '新闻', '上传代码', '展示代码'],
+      page:['/','Login','SignUp','About','NewsArticles','GroupIntroduce','News','updateCode','showCode']
     };
   },
   methods: {
-    handleClick() {
-      console.log(this.activeName);
-      console.log(this.$route.name);
+    tabClick() {
+      for (let i = 0; i < this.site.length; i++) {
+        if (this.activeName === this.site[i])
+          this.$router.push(this.page[i]);
+      }
     },
   },
 };
