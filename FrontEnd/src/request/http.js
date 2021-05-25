f7cc58b7e3e9e10435cb3bff3292116df5ddad5c
大饼 <710374839@qq.com>
2021-05-25 17:08:04 +0800

    简化部分代码

--- FrontEnd/src/request/http.js
+++ FrontEnd/src/request/http.js
@@ -4,6 +4,13 @@ import router from "../router";
 import { Message } from "element-ui";
 // import qs from "qs";
 
+const message = (text) => {
+  Message({
+    message: text,
+    type: "error",
+    duration: 1000,
+  });
+};
 var instance = axios.create({ timeout: 1000 * 12 });
 instance.defaults.headers.post["Content-Type"] =
   "application/x-www-form-urlencoded";
@@ -37,29 +44,17 @@ instance.interceptors.response.use(
         case 400:
           switch (error.response.status.ErrorCode) {
             case 100000:
-              Message({
-                message: "格式错误,请重试",
-                type: "warning",
-                duration: 1000,
-              });
+              message("格式错误,请重试");
               break;
             case 100101:
-              Message({
-                message: "账号密码不正确",
-                type: "error",
-                duration: 1000,
-              });
+              message("账号密码不正确");
               break;
 
             // 登录过期对用户进行提示
             // 清除本地token和清空vuex中token对象
             // 跳转登录页面
             case 100102:
-              Message({
-                message: "登录过期，请重新登录",
-                type: "error",
-                duration: 1000,
-              });
+              message("登录过期，请重新登录");
               // 清除token
               store.commit("tokenSet", null);
               // 跳转登录页面，并将要浏览的页面fullPath传过去，登录成功后跳转需要访问的页面
@@ -88,19 +83,11 @@ instance.interceptors.response.use(
 
         // 404请求不存在
         case 404:
-          Message({
-            message: "网络请求不存在",
-            type: "error",
-            duration: 1500,
-          });
+          message("网络请求不存在");
           break;
         // 其他错误，直接抛出错误提示
         default:
-          Message({
-            type: "error",
-            message: error.response.data.message,
-            duration: 1500,
-          });
+          message(error.response.data.message);
       }
       return Promise.reject(error.response);
     }
